5d762e1a7149bb63e48bd528d0dc7ae252ddbd2f
Soren <sorenisme@gmail.com>
2022-09-22 11:43:57 -0400

    commit tree 26007af4f617739e03ceaf7f0863dac0a894dbce

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = []
+
+function writeCards(names, event) {
+    let newArr = []
+    for(let i = 0; i < names.length; i++){
+    let newEnt = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    newArr.push(newEnt)
+    }
+
+    return newArr;
+}
+
+const countDown = (number) =>{
+    while(number >= 0){
+        console.log(number--)
+    }
+}
